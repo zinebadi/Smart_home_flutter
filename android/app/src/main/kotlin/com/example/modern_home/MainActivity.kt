065fd9a8747d93b6c9a0fcefeef8ce13d8144a92
Zine <zinebeloubbadi@MacBook-Pro.local>
2022-12-04 18:11:40 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/modern_home/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.modern_home
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:modern_home/util/smart_device_box.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+//padding constants
+  final double horizontalPadding = 40;
+  final double verticalPadding = 25;
+
+  //List of Smart devices
+  List mySmartDevices = [
+    // [ smartDeviceName, iconPath, powerStatus ]
+    ['Smart Light', 'lib/icons/light-bulb.png', true],
+    ['Smart AC', 'lib/icons/air-conditioner.png', false],
+    ['Smart TV', 'lib/icons/smart-tv.png', false],
+    ['Smart Fan', 'lib/icons/fan.png', false],
+  ];
+
+  // power button changed
+  void powerSwitchChanged(bool value, int index) {
+    setState(() {
+      mySmartDevices[index][2] = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //custom app bar
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: horizontalPadding, vertical: verticalPadding),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //menu icon
+                  Image.asset(
+                    'lib/icons/menu.png',
+                    height: 45,
+                    color: Colors.grey[800],
+                  ),
+
+                  // account icon
+                  Icon(
+                    Icons.person,
+                    size: 45,
+                    color: Colors.grey[800],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            //welcome home MITCH KOKO
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome Home,',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                  Text(
+                    'MITCH KOKO',
+                    style: GoogleFonts.bebasNeue(
+                      fontSize: 72,
+                      
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Divider(
+                color: Colors.grey[400],
+                thickness: 1,
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            //smart devices + grid
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Text(
+                'Smart Devices',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+            Expanded(
+              child: GridView.builder(
+                itemCount: mySmartDevices.length,
+                physics: const NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.all(25),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1 / 1.3,
+                ),
+                itemBuilder: (context, index) {
+                  return SmartDeviceBox(
+                    smartDeviceName: mySmartDevices[index][0],
+                    iconPath: mySmartDevices[index][1],
+                    powerOn: mySmartDevices[index][2],
+                    onChanged: (value) => powerSwitchChanged(value, index),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
